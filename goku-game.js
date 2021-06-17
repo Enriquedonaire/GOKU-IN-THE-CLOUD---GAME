f2c9b4dbf68f5a7e80e2e186c7cd46e3e53638bc
Enrique Donaire <donaire.q2@gmail.com>
2021-06-17 11:09:13 +0200

    Goku in the Cloud

--- goku-game.js
+++ goku-game.js
@@ -49,7 +49,6 @@ let sphere6 = new Image
 sphere6.src = 'Images/Sphere6.png'
 let sphere7 = new Image
 sphere7.src = 'Images/Sphere7.png'
-let myFonts = new font('Fonts/Saiyan-Sans')
 let gokuX = 400, gokuY = 250
 let incrX = 0, incrY = - 2
 let isUp = false, isDown = false, isRight = false, isLeft = false;
@@ -220,11 +219,11 @@ function draw(){
 
       
         ctx.fillStyle = 'orange'
-        ctx.font = '75px Fonts/Saiyan-Sans.ttf'
+        ctx.font = '75px Fonts/Saiyan-Sans'
         ctx.fillText(`Score: ${score}`, 550, 85)
       
         ctx.fillStyle = 'blue'
-        ctx.font = '73px Fonts/Saiyan-Sans.ttf'
+        ctx.font = '73px Fonts/Saiyan-Sans'
         ctx.fillText(`Enemy: ${EnemiesCollision}`, 850, 85)
      
 
